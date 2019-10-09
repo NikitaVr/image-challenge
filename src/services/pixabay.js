81d337df707a4c18bae0545882bb4e8840563cf1
NikitaVr <nvarabei@gmail.com>
2019-10-08 21:33:22 -0400

    cleanup

--- src/services/pixabay.js
+++ src/services/pixabay.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 async function getImages(query) {
     const response = await axios.get('https://pixabay.com/api/', {
         params: {
-            key: "13880753-5717f7f6e7607e00db6390228",
+            key: "",
             q: query
         }
     });
